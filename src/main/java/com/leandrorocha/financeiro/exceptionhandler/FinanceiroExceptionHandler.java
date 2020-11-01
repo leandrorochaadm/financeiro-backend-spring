4a86a10a545d633ec2ec3523af96f2543312bb4c
leandrorochaadm <leandrorochaadm@gmail.com>
2020-11-01 14:14:17 -0400

    tratamento de erros

---
+++ src/main/java/com/leandrorocha/financeiro/exceptionhandler/FinanceiroExceptionHandler.java
@@ -0,0 +1,64 @@
+package com.leandrorocha.financeiro.exceptionhandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class FinanceiroExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Autowired
+	private MessageSource messageSource;
+	
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvoldedor = ex.getCause().toString();
+		return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDesenvoldedor), headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	@ExceptionHandler({EmptyResultDataAccessException.class})
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void handleEmptyResultDataAccessException(RuntimeException ex) {
+		/*
+		 * String mensagemUsuario = messageSource.getMessage("mensagem.naoexiste", null,
+		 * LocaleContextHolder.getLocale()); String mensagemDesenvoldedor =
+		 * ex.getCause().toString(); return handleExceptionInternal(ex, new
+		 * Erro(mensagemUsuario, mensagemDesenvoldedor), null, HttpStatus.NOT_FOUND,
+		 * null);
+		 */
+	}
+	
+	public static class Erro{
+		private String mensagemUsuario;
+		private String mensagemDesenvolvedor;
+		
+		public Erro(String mensagemUsuario, String mensagemDesevolvedor) {
+			this.mensagemUsuario = mensagemUsuario;
+			this.mensagemDesenvolvedor = mensagemDesevolvedor;
+		}
+
+		public String getMensagemUsuario() {
+			return mensagemUsuario;
+		}
+
+		public String getMensagemDesenvolvedor() {
+			return mensagemDesenvolvedor;
+		}
+
+	}
+}
